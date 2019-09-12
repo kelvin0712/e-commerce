405d3f3430af9fcb8eeb7d8e32549e887134de6b
Kelvin <duchung97dl@gmail.com>
2019-09-12 16:58:58 +1000

    add header and collection items page

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.styles.scss';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+
+const Header = () => (
+    <div className="header">
+        <Link className="logo-container" to="/">
+            <Logo className='logo' />
+        </Link>
+        <div className="options">
+            <Link className="option" to="/shop">Shop</Link>
+            <Link className="option" to="/">Contact</Link>
+        </div>
+    </div>
+
+);
+
+export default Header;
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './preview-collection.styles.scss';
 
 const PreviewCollection = ({title, items}) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview"> 
-            {items.filter((el, index) => index < 4).map((el) => <div key={el.id}>{el.name}</div>)}
+            {items.filter((el, index) => index < 4).map(({id, ...otherItemProps}) => <CollectionItem key={id} {...otherItemProps}/>)}
         </div>    
     </div>
 )
