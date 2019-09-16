732c54cb14161a102a0207b40a8d0c83874adb3b
Kelvin <duchung97dl@gmail.com>
2019-09-17 08:48:20 +1000

    implemented firebase untils, included ability to store users into firestore

--- src/firebase/firebase.ultils.js
+++ src/firebase/firebase.ultils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:1064869029906:web:4d7eed49a00c52d54890cb"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createDate = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createDate,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating users", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react'; 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUp = () => (
-    <div><SignIn /></div>
+    <div className="sign-in-and-sign-up"><SignIn /><SignUp /></div>
 )
 
 export default SignInAndSignUp;
