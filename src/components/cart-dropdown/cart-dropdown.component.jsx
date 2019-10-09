d42a3502170df3b9072315a786989fe6a8f46d46
Kelvin <duchung97dl@gmail.com>
2019-10-10 09:05:41 +1000

    update our application with Reselect, replace cart-selector with memoized selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 import { connect } from 'react-redux';
 
 import './cart-dropdown.styles.scss';
@@ -17,8 +18,8 @@ const CartDropDown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems: cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
     
 export default connect(mapStateToProps)(CartDropDown); 
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ReactComponent as ShoppingBag} from '../../assets/shopping-bag.svg';
-
+import {selectCartItemCount} from '../../redux/cart/cart.selectors';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
@@ -18,10 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce(
-        (accumulator, cartItem) => accumulator + cartItem.quantity, 0
-    )
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemCount(state)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
